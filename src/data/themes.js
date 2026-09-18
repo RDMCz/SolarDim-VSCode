934a373b314f91121d0b1d22fe050654cad72f40
RDMCz <dadun001@email.cz>
2026-09-18 23:02:14 +0200

    0.1.0

--- src/data/themes.js
+++ src/data/themes.js
@@ -3,7 +3,6 @@ const themes = {
     semanticHighlighting: true,
 
     type: {
-        dark: "dark",
         light: "light",
     },
 
@@ -11,14 +10,13 @@ const themes = {
         black: "#000000",
         white: "#FFFFFF",
 
-        base_f_6: "#001E26",
+        //base_f_6: "#001E26",
         base_f_5: "#00212B",
-        base_f_4: "#002731",
-        base_f_3: "#002B36",
-        base_f_2: "#073642",
+        //base_f_4: "#002731",
+        //base_f_3: "#002B36",
+        //base_f_2: "#073642",
         base_f_1: "#4F6169",
 
-        base_b_0: "#839496",
         base_b_1: "#879493",
         base_b_2: "#E1DFD8",
         base_b_3: "#F0EEE6",
@@ -28,19 +26,15 @@ const themes = {
         red0: "#DC322F",
         red1: "#D30102",
         red2: "#C53441",
-        magenta0: "#C52B75",
-        magenta1: "#AB395B",
+        magenta: "#C52B75",
         violet: "#676BC2",
         indigo: "#644EA0",
         blue: "#237DBE",
         cyan: "#27968D",
         green: "#748502",
-        teal0: "#005A6F",
-        teal1: "#004454",
-        teal2: "#003847",
+        teal: "#00627A",
 
         light: {
-            base0: "#BBBBBB",
             cream: "#D0CDC0",
             gold0: "#D2C58D",
             gold1: "#AC9D57",
@@ -55,10 +49,12 @@ const themes = {
 
             misc1: "#723021",
             misc3: "#DDD8C3",
-            misc4: "#78489D40",
-            misc5: "#8D8C8C90",
-            misc6: "#4B4426",
-            misc7: "#4B4426AA"
+            misc4: "#78489D",
+            misc5: "#8D8C8C",
+            misc7: "#4B4426",
+
+            ansiBrightBlue: "#839496",
+            ansiRed: "#D2212D",
         },
 
     }
